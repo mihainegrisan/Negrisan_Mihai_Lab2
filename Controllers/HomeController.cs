ce1532fbcb9033f1fa6e5242cd732ee3a9a8560d
Mihai Negrisan <mihainegrisan1@gmail.com>
2021-10-21 20:36:25 +0300

    Added Lab4

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Negrisan_Mihai_Lab2.Data;
 using Negrisan_Mihai_Lab2.Models;
+using Negrisan_Mihai_Lab2.Models.LibraryViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Negrisan_Mihai_Lab2.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly LibraryContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, LibraryContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -32,6 +37,19 @@ namespace Negrisan_Mihai_Lab2.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<ActionResult> Statistics()
+        {
+            IQueryable<OrderGroup> data =
+                from order in _context.Orders
+                group order by order.OrderDate into dateGroup
+                select new OrderGroup()
+                {
+                    OrderDate = dateGroup.Key,
+                    BookCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
     }
 }
--- Models/Book.cs
+++ Models/Book.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,8 +12,12 @@ namespace Negrisan_Mihai_Lab2.Models
         public int ID { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+
+        [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
+
         public ICollection<Order> Orders { get; set; }
+        public ICollection<PublishedBook> PublishedBooks { get; set; }
 
     }
     
